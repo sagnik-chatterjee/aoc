63444a850cddf7cac249f95365916e237b06ac06
Sagnik Chatterjee <sagnik.chatterjee51@gmail.com>
2021-06-19 12:19:29 +0530

    solved part1.py

--- 2015/day5/part1.py
+++ 2015/day5/part1.py
@@ -1,59 +1,75 @@
+from os import stat
+from sys import flags
 
-def part1(str1):
-    count=0
-    status=False
-    for i in str1:
-        if i=='a' or i=='e' or i=='i' or i=='o' or i=='u':
-            count+=1
-            if count ==3:
-                status=True
-                break 
-    return status 
 
-def part2(str1):
-    status=False
-    count=0
-    for i in range(len(str1)-1):
-        if str1[i] == str1[i+1]:
-            count+=1
-        
-        if count ==1 :
-            status=True
-            break 
-    return status 
+class Solution:
+    def __init__(self, str_input):
+        self.str_input = str_input
 
-def part3(str1):
-    status=True
-    for i in range(len(str1)-1):
-        if str1[i]!='a' and str1[i+1]!='b':
-            status=False
-            break
-        elif str1[i]!='c' and str1[i+1]!='d':
-            status=False
-            break
-        elif str1[i]!='p' and str1[i+1]!='q':
-            status=False
-            break
-        elif str1[i]!='x' and str1[i]!='y':
-            status=False
-            break 
-    return status        
-                        
+    def part1(self):
+        """
+        checks condn that at least three voiewls present
+        """
+        status = False
+        count = 0
+        for i in self.str_input:
+            if i == "a" or i == "e" or i == "i" or i == "o" or i == "u":
+                count += 1
+        if count >= 3:
+            status = True
+        return status
+
+    def part2(self):
+        """
+        contains one letter that apperas twice in a row
+        xx ,dd,aabbccdd etc.
+        """
+        status = False
+        count = 0
+        for i in range(len(self.str_input) - 1):
+            if self.str_input[i] == self.str_input[i + 1]:
+                count += 1
+
+        if count >= 1:
+            status = True
+        return status
+
+    def part3(self):
+        """
+        does not conatins the strings ab ,cd ,pq or xy even if they are part of one of the
+        other requirements
+        """
+        status = True
+        for i in range(len(self.str_input) - 1):
+            if self.str_input[i] == "a" and self.str_input[i + 1] == "b":
+                status = False
+                break
+            elif self.str_input[i] == "c" and self.str_input[i + 1] == "d":
+                status = False
+                break
+            elif self.str_input[i] == "p" and self.str_input[i + 1] == "q":
+                status = False
+                break
+            elif self.str_input[i] == "x" and self.str_input[i + 1] == "y":
+                status = False
+                break
+
+        return status
 
 
 def main():
-    input_stream=[]
-    with open("input.txt") as f:
-        inp= f.readlines()
-        input_stream=[i.strip() for i in inp]
-    
-    nice_strings=0
+    input_stream = []
+    with open("input.txt", "r") as f:
+        inp = f.readlines()
+        input_stream = [i.strip() for i in inp]
+    count = 0
     for i in input_stream:
-        if part1(i) and part2(i) and part3(i):
-            nice_strings+=1
-        
-    print(nice_strings)
+        p = Solution(i)
+        if p.part1() and p.part2() and p.part3():
+            count += 1
+
+    print(count)
 
 
-
-main()
+if __name__ == "__main__":
+    main()
